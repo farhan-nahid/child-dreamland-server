daa1792fef330a6672038645e2cefcb43167718e
gesora96 <nahidahmed9933@gmail.com>
2022-01-27 19:40:03 +0600

    some api created

--- index.js
+++ index.js
@@ -49,12 +49,89 @@ async function run() {
     const orderCollection = database.collection('orders');
     const userCollection = database.collection('users');
 
+    /* 
+    
+                                                   GET APIS
+    
+    */
+
+    // GET ADMIN OR NOT?
+
+    app.get('/user/:email', async (req, res) => {
+      const query = { email: req.params.email };
+      const user = await userCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'Admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // GET ALL COURSES
+
+    app.get('/all-courses', async (req, res) => {
+      const cursor = courseCollection.find({});
+      const courses = await cursor.toArray();
+      res.json(courses);
+    });
+
+    // GET A SINGLE COURSE
+
+    app.get('/course/:id', async (req, res) => {
+      const query = { _id: ObjectId(req.params.id) };
+      const course = await courseCollection.findOne(query);
+      res.json(course);
+    });
+
+    // GET SINGLE USERS
+
+    app.get('/normal-users/:email', async (req, res) => {
+      const cursor = await userCollection.findOne({ email: req.params.email });
+      res.json(cursor);
+    });
+
+    // GET ALL USERS
+
+    app.get('/all-users', verifyJwtToken, async (req, res) => {});
+
+    /* 
+    
+                                                   POST APIS
+    
+    */
+
     // POST A USER
 
     app.post('/user', async (req, res) => {
       const result = await userCollection.insertOne(req.body);
       res.json(result);
     });
+
+    // POST A SINGLE COURSES
+
+    app.post('/add-course', async (req, res) => {
+      const result = await courseCollection.insertOne(req.body);
+      res.json(result);
+    });
+
+    // POST A SINGLE ORDER
+
+    app.post('/add-order', async (req, res) => {
+      const result = await orderCollection.insertOne(req.body);
+      res.json(result);
+    });
+
+    /* 
+    
+                                                   DELETE APIS
+    
+    */
+
+    /* 
+    
+                                                   UPDATE APIS
+    
+    */
 
     // PUT admin & check with JWT Token  he/she is admin or not ?
 
@@ -72,40 +149,6 @@ async function run() {
       } else {
         req.status(401).json({ message: 'You do not have access to make admin' });
       }
-    });
-
-    // GET ADMIN OR NOT?
-
-    app.get('/user/:email', async (req, res) => {
-      const query = { email: req.params.email };
-      const user = await userCollection.findOne(query);
-      let isAdmin = false;
-      if (user?.role === 'Admin') {
-        isAdmin = true;
-      }
-      res.json({ admin: isAdmin });
-    });
-
-    // POST A SINGLE courses
-
-    app.post('/add-course', async (req, res) => {
-      const result = await courseCollection.insertOne(req.body);
-      res.json(result);
-    });
-
-    // GET ALL courses
-
-    app.get('/all-courses', async (req, res) => {
-      const cursor = courseCollection.find({});
-      const courses = await cursor.toArray();
-      res.json(courses);
-    });
-
-    // POST A SINGLE ORDER
-
-    app.post('/add-order', async (req, res) => {
-      const result = await orderCollection.insertOne(req.body);
-      res.json(result);
     });
   } finally {
     // client.close()
